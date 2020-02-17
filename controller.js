149dc5770ea9399fca2eae9c24f9db7bbe0abad6
Raktim Shrestha <rakimsth@gmail.com>
2020-02-18 00:00:44 +0545

    Create controller.js
    updating controller

---
+++ controller.js
@@ -0,0 +1,15 @@
+const messenger = require("../messenger");
+
+class Controller {
+
+  async sendEmail(payload) {
+    let response = await messenger.send({
+      to: "receiver@mail.com", //change it
+      template: "queryEmail",
+      data: payload //alter it as your need
+    });
+    return response;
+  }
+}
+
+module.exports = new Controller();
